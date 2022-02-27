132ff59e491ae89fda832261ff0955b4067f72fc
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-02-27 19:00:54 +0200

    render StackContent

--- src/components/lostCitiesRivals/players/PlayerCard.component.tsx
+++ src/components/lostCitiesRivals/players/PlayerCard.component.tsx
@@ -1,6 +1,22 @@
-import { Paper } from "@mui/material";
+import { Paper, Stack, Typography } from "@mui/material";
+import { StackContent } from "../stacks/StackContent.component";
 import { PlayerI } from "./player.type";
 
 export const PlayerCard: React.FC<{ player: PlayerI }> = ({ player }) => {
-  return <Paper sx={{ p: 2 }}>{player.name}</Paper>;
+  return (
+    <Paper sx={{ p: 1 }}>
+      <Stack spacing={1} alignItems="center">
+        <Typography variant="h6">{player.name}</Typography>
+
+        {player.stacks.map((stack, index) => (
+          <StackContent
+            stack={stack}
+            name={`Stack ${index + 1}`}
+            playerId={player.id}
+            key={stack.id}
+          />
+        ))}
+      </Stack>
+    </Paper>
+  );
 };

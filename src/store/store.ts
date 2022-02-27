3d323304895555b6bed20c1a7e42400c233322ce
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-02-27 22:27:10 +0200

    persist store to localStorage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { reducer as lostCitiesRivalsReducer } from "../components/lostCitiesRivals/lostCitiesRivals.slice";
+import { loadState, saveState } from "./localStorage";
 
 export const store = configureStore({
   reducer: {
     lostCitiesRivals: lostCitiesRivalsReducer,
   },
+  preloadedState: loadState(),
+});
+
+store.subscribe(() => {
+  saveState(store.getState());
 });
 
 export type RootState = ReturnType<typeof store.getState>;

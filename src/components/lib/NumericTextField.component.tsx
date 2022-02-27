21ad1860f2660fe9aac8914c7f3094f424ffadf0
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-02-27 23:49:27 +0200

    add NaN handling in NumericTextField

--- src/components/lib/NumericTextField.component.tsx
+++ src/components/lib/NumericTextField.component.tsx
@@ -1,18 +1,28 @@
-import { Stack, TextField, TextFieldProps } from "@mui/material";
+import { TextField, TextFieldProps } from "@mui/material";
 
 export type NumericTextFieldProps = TextFieldProps;
 
 export const NumericTextField: React.FC<NumericTextFieldProps> = (props) => {
   return (
-    <Stack direction="row">
-      <TextField
-        {...props}
-        inputProps={{
-          inputMode: "numeric",
-          pattern: "[0-9]*",
-          ...props.inputProps,
-        }}
-      />
-    </Stack>
+    <TextField
+      {...props}
+      inputProps={{
+        inputMode: "numeric",
+        pattern: "[0-9]*",
+        ...props.inputProps,
+      }}
+      onChange={(e) => {
+        const value = +e.target.value;
+
+        if (isNaN(value)) {
+          return;
+        }
+
+        props.onChange?.(e);
+      }}
+      onFocus={(event) => {
+        event.target.select();
+      }}
+    />
   );
 };

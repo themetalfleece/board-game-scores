8c00cff30ac2e602a2e75cbcd3876ac6e3a5d4f9
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-02-27 19:50:26 +0200

    render PlayerDeleteButton

--- src/components/lostCitiesRivals/players/PlayerCard.component.tsx
+++ src/components/lostCitiesRivals/players/PlayerCard.component.tsx
@@ -1,12 +1,16 @@
 import { Paper, Stack, Typography } from "@mui/material";
 import { StackContent } from "../stacks/StackContent.component";
 import { PlayerI } from "./player.type";
+import { PlayerDeleteButton } from "./PlayerDeleteButton.component";
 
 export const PlayerCard: React.FC<{ player: PlayerI }> = ({ player }) => {
   return (
     <Paper sx={{ p: 1 }}>
       <Stack spacing={1} alignItems="center">
-        <Typography variant="h6">{player.name}</Typography>
+        <Stack direction="row" alignItems="center">
+          <Typography variant="h6">{player.name}</Typography>
+          <PlayerDeleteButton playerId={player.id} />
+        </Stack>
 
         {player.stacks.map((stack, index) => (
           <StackContent

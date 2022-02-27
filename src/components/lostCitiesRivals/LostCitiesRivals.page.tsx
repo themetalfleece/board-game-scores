ba5821980f559d9239cc22839ce3f2a3d090bc1a
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-02-27 17:13:02 +0200

    render PlayerCard, AddPlayerButton in LostCitiesRivals

--- src/components/lostCitiesRivals/LostCitiesRivals.page.tsx
+++ src/components/lostCitiesRivals/LostCitiesRivals.page.tsx
@@ -1,11 +1,22 @@
+import { Stack, Typography } from "@mui/material";
+import { AddPlayerButton } from "./players/AddPlayerButton.component";
+import { PlayerCard } from "./players/PlayerCard.component";
 import { useSelector } from "./useSelector.hook";
 
 export const LostCitiesRivals: React.FC<{}> = () => {
   const state = useSelector();
 
+  const { players } = state;
+
   return (
-    <div>
-      Lost Cities Rivals {JSON.stringify(state.players)}
-    </div>
+    <Stack alignItems="center" spacing={2}>
+      <Typography variant="h5">Lost Cities Rivals</Typography>
+
+      {players.map((player) => (
+        <PlayerCard player={player} key={player.id} />
+      ))}
+
+      <AddPlayerButton />
+    </Stack>
   );
 };

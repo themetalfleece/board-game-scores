11ae886cf9847a74faeb45ea2ff84cdb5a1e2ec4
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-02-28 00:07:31 +0200

    add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Box } from "@mui/material";
 import { RoutedGames } from "./components/routedGames/RoutedGames.router";
+import { theme } from "./theme/theme";
 
 function App() {
   return (
-    <CssBaseline>
-      <Box m={2}>
-        <RoutedGames />
-      </Box>
-    </CssBaseline>
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Box m={2}>
+          <RoutedGames />
+        </Box>
+      </CssBaseline>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/theme.tsx
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#90caf9",
+    },
+    secondary: {
+      main: "#f48fb1",
+    },
+    background: {
+      default: "#212121",
+      paper: "#424242",
+    },
+  },
+});

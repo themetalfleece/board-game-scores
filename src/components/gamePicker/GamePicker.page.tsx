7172a4594b9387793146ef20cb1e7f350e343293
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-02-27 17:09:52 +0200

    replace GamePicker Box with Stack

--- src/components/gamePicker/GamePicker.page.tsx
+++ src/components/gamePicker/GamePicker.page.tsx
@@ -1,20 +1,15 @@
-import { Typography, Box } from "@mui/material";
+import { Typography, Stack } from "@mui/material";
 import { GameChooseButton } from "../gameChooseButton/GameChooseButton.component";
 import { gameRoutes } from "../routedGames/gameRoutes";
 
 export const GamePicker: React.FC<{}> = () => {
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-      sx={{ gap: (theme) => theme.spacing(1) }}
-    >
+    <Stack alignItems="center" sx={{ gap: (theme) => theme.spacing(1) }}>
       <Typography variant="h4">Choose your game</Typography>
+
       <GameChooseButton gamePath={gameRoutes.lostCitiesRivals}>
         Lost Cities Rivals
       </GameChooseButton>
-    </Box>
+    </Stack>
   );
 };

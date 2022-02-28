6549179e83bdd84c3341b073d78367fa9cb47648
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-02-28 22:08:19 +0200

    render PlayerScore

--- src/components/ticketToRideEurope/players/PlayerCard.component.tsx
+++ src/components/ticketToRideEurope/players/PlayerCard.component.tsx
@@ -2,12 +2,15 @@ import { Paper, Stack, Typography } from "@mui/material";
 import { PlayerI } from "./player.type";
 import { PlayerContols } from "./PlayerControls.component";
 import { PlayerDeleteButton } from "./PlayerDeleteButton.component";
+import { PlayerScore } from "./PlayerScore";
 
 export const PlayerCard: React.FC<{ player: PlayerI }> = ({ player }) => {
   return (
     <Paper sx={{ p: 1, width: "300px" }}>
       <Stack spacing={1} alignItems="center">
         <Typography variant="h6">{player.name}</Typography>
+
+        <PlayerScore player={player} />
 
         <PlayerContols player={player} />
 

b74d1818aa3c6da1fc55d8a7e424e4be8657f262
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-02-28 18:49:44 +0200

    render PlayerCard for players

--- src/components/ticketToRideEurope/TicketToRideEurope.page.tsx
+++ src/components/ticketToRideEurope/TicketToRideEurope.page.tsx
@@ -1,9 +1,21 @@
-import { Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import { PlayerCard } from "./players/PlayerCard.component";
+import { useSelector } from "./useSelector.hook";
 
 export const TicketToRideEurope: React.FC<{}> = () => {
+  const state = useSelector();
+
+  const { players } = state;
+
   return (
     <Stack alignItems="center" spacing={2}>
       <Typography variant="h5">Ticket to Ride Europe</Typography>
+
+      <Box display="flex" flexWrap="wrap" justifyContent="center" gap={2}>
+        {players.map((player) => (
+          <PlayerCard player={player} key={player.id} />
+        ))}
+      </Box>
     </Stack>
   );
 };

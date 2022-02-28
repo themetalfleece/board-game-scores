89a041bc8c8ac40081caf1a3c617b9039abdb812
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-02-28 22:51:04 +0200

    add delete confirmation

--- src/components/lostCitiesRivals/players/PlayerDeleteButton.component.tsx
+++ src/components/lostCitiesRivals/players/PlayerDeleteButton.component.tsx
@@ -3,16 +3,29 @@ import { PlayerI } from "../players/player.type";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useAppDispatch } from "../../../store/useAppDispatch.hook";
 import { deletePlayer } from "../lostCitiesRivals.slice";
+import { useState } from "react";
 
 export const PlayerDeleteButton: React.FC<{ playerId: PlayerI["id"] }> = ({
   playerId,
 }) => {
   const dispatch = useAppDispatch();
 
-  const handleDelete = () => dispatch(deletePlayer(playerId));
+  const [deletePending, setDeletePending] = useState(false);
+
+  const handleDelete = () => {
+    if (deletePending) {
+      dispatch(deletePlayer(playerId));
+    } else {
+      setDeletePending(true);
+    }
+  };
 
   return (
-    <IconButton color="error" onClick={handleDelete}>
+    <IconButton
+      color={deletePending ? "error" : "default"}
+      onClick={handleDelete}
+      onBlur={() => setDeletePending(false)}
+    >
       <DeleteIcon />
     </IconButton>
   );

c55550f1341d5daded0620e1dd1f89c025423336
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-02-28 18:28:31 +0200

    add TicketToRideEurope page

---
+++ src/components/ticketToRideEurope/TicketToRideEurope.page.tsx
@@ -0,0 +1,9 @@
+import { Stack, Typography } from "@mui/material";
+
+export const TicketToRideEurope: React.FC<{}> = () => {
+  return (
+    <Stack alignItems="center" spacing={2}>
+      <Typography variant="h5">Ticket to Ride Europe</Typography>
+    </Stack>
+  );
+};

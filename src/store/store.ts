ad06c812e612c11dbffc1aa8f6e7a04ce5d7ac04
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-03-13 12:25:45 +0200

    add ohanami reducer to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { reducer as lostCitiesRivalsReducer } from "../components/lostCitiesRivals/lostCitiesRivals.slice";
 import { reducer as ticketToRideEuropeReducer } from "../components/ticketToRideEurope/ticketToRideEurope.slice";
+import { reducer as ohanamiReducer } from "../components/ohanami/ohanami.slice";
 import { loadState, saveState } from "./localStorage";
 
 export const store = configureStore({
   reducer: {
     lostCitiesRivals: lostCitiesRivalsReducer,
     ticketToRideEurope: ticketToRideEuropeReducer,
+    ohanami: ohanamiReducer,
   },
   preloadedState: loadState(),
 });

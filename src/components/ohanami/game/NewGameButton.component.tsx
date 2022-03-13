e7031f86969135bea6b737d6d4161d721fdf0706
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-03-13 12:25:23 +0200

    add ohanami NewGameButton

---
+++ src/components/ohanami/game/NewGameButton.component.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+import { useAppDispatch } from "../../../store/useAppDispatch.hook";
+import { reset } from "../ohanami.slice";
+
+export const NewGameButton: React.FC<{}> = () => {
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => dispatch(reset());
+
+  return <Button onClick={handleClick}>New Game</Button>;
+};

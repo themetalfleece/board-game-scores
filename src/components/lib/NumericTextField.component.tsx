45596eb68a9ee7d9165f56344d445de268840c52
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-07-05 19:55:24 +0300

    make NumericTextField type number, add min prop

--- src/components/lib/NumericTextField.component.tsx
+++ src/components/lib/NumericTextField.component.tsx
@@ -1,14 +1,19 @@
 import { TextField, TextFieldProps } from "@mui/material";
 
-export type NumericTextFieldProps = TextFieldProps;
+export type NumericTextFieldProps = TextFieldProps & {
+  /** default 0 */
+  min?: number;
+};
 
 export const NumericTextField: React.FC<NumericTextFieldProps> = (props) => {
   return (
     <TextField
       {...props}
+      type="number"
       inputProps={{
         inputMode: "numeric",
         pattern: "[0-9]*",
+        min: typeof props.min === "number" ? props.min : 0,
         ...props.inputProps,
       }}
       onChange={(e) => {

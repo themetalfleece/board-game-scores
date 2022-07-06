4d217f278fecc0a55851b1d134c8721e05c306f9
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-07-06 15:30:37 +0300

    fix typing issue

--- src/components/gamePicker/GameChooseButton.component.tsx
+++ src/components/gamePicker/GameChooseButton.component.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@mui/material";
+import React from "react";
 import { useNavigate } from "react-router";
 
 /** a button which navigates to the corresponding board game page on click */
-export const GameChooseButton: React.FC<{ gamePath: string }> = ({
-  gamePath,
-  children,
-}) => {
+export const GameChooseButton: React.FC<{
+  gamePath: string;
+  children: React.ReactNode;
+}> = ({ gamePath, children }) => {
   const navigate = useNavigate();
   return (
     <Button
